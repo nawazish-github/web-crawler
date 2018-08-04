235f714dc8786374d1ee99452a3a87aef4b6234d
mnawaz <mnawaz@thoughtworks.com>
2018-08-05 01:16:42 +0530

    Request URL for its HTML content

--- main/web-crawler.go
+++ main/web-crawler.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
 
 var urlReg = make(map[string][]string)
@@ -16,6 +19,12 @@ func main() {
 		return
 	}
 	addURLToURLRegistry(rawURL)
+	resp, err := requestTheURL(rawURL)
+	if err != nil {
+		log.Fatal("Request Failure: ", err)
+		return
+	}
+	defer resp.Body.Close()
 }
 
 //ParseURL parses a raw URL
@@ -29,4 +38,14 @@ func addURLToURLRegistry(rawURL string) {
 	if _, ok := urlReg[rawURL]; !ok {
 		urlReg[rawURL] = []string{}
 	}
+}
+
+func requestTheURL(rawURL string) (*http.Response, error) {
+	resp, err := http.Get(rawURL)
+	contentType := resp.Header.Get("Content-Type")
+	if !strings.HasPrefix(contentType, "text/html") {
+		errMsg := "Incompatible Mime Type. Expecting text/html got "
+		return nil, errors.New(errMsg + resp.Header.Get("Content-Type"))
+	}
+	return resp, err
 }
