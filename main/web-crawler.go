1a29c7e3e7ece14d1f82ec972f73fae6c11be68c
mnawaz <mnawaz@thoughtworks.com>
2018-08-05 01:12:40 +0530

    Add each source visited URL to the registry

--- main/web-crawler.go
+++ main/web-crawler.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+var urlReg = make(map[string][]string)
+
 func main() {
 	rawURL := os.Args[1]
 	_, err := ParseURL(rawURL)
@@ -13,10 +15,18 @@ func main() {
 		log.Fatal("Parse failure")
 		return
 	}
+	addURLToURLRegistry(rawURL)
 }
 
 //ParseURL parses a raw URL
 func ParseURL(rawURL string) (*url.URL, error) {
 	pURL, err := url.Parse(rawURL)
 	return pURL, err
+}
+
+//addURLToURLRegistry maintains a registry of crawled URLs
+func addURLToURLRegistry(rawURL string) {
+	if _, ok := urlReg[rawURL]; !ok {
+		urlReg[rawURL] = []string{}
+	}
 }
