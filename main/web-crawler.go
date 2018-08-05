7fd1a7226487ac051d3bf6151fb012490354c026
mnawaz <mnawaz@thoughtworks.com>
2018-08-06 00:56:30 +0530

    Receive depth from commandline as the last argument

--- main/web-crawler.go
+++ main/web-crawler.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/nawazish-github/web-crawler/crawler"
 )
 
 func main() {
 	rawURL := os.Args[1]
+	depth, err := strconv.Atoi(os.Args[len(os.Args)-1])
+	if err != nil {
+		crawler.Depth = 1
+	}
+	crawler.Depth = depth
+
+	log.Println(rawURL)
+	log.Println(crawler.Depth)
 	rawURLs := []string{rawURL}
-	crawler.MultiCrawler(rawURLs)
+	gens := crawler.MultiCrawler(rawURLs)
+	for _, gen := range gens {
+		log.Println(gen)
+	}
 }
