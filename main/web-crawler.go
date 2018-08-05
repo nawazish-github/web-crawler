0196ed6d80cdc828b45dcd1eb61f9b17c99d8ae7
mnawaz <mnawaz@thoughtworks.com>
2018-08-05 23:51:00 +0530

    Remove unnecessary code from the main function

--- main/web-crawler.go
+++ main/web-crawler.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"log"
 	"os"
 
-	"github.com/nawazish-github/web-crawler/httphelper"
-
-	"github.com/nawazish-github/web-crawler/model"
-
-	"github.com/nawazish-github/web-crawler/hrefhandler"
-
-	"github.com/nawazish-github/web-crawler/parsers"
+	"github.com/nawazish-github/web-crawler/crawler"
 )
-
-const depth = 1
 
 var (
 	isFirstItr = true
@@ -22,30 +13,6 @@ var (
 
 func main() {
 	rawURL := os.Args[1]
-	for i := 0; i < depth; i++ {
-		//allow first iteration to execute without any checks.
-		if !isFirstItr && len(urlReg[rawURL]) == 0 {
-			break
-		}
-		pURL, urlParseErr := parsers.ParseURL(rawURL)
-		if urlParseErr != nil {
-			log.Fatal("URL Parse Error: ", urlParseErr)
-			continue
-		}
-		isFirstItr = false
-		model.AddURLToURLRegistry(rawURL)
-		resp, err := httphelper.RequestTheURL(rawURL)
-		if err != nil {
-			log.Fatal("Request Failure: ", err)
-			continue
-		}
-		defer resp.Body.Close()
-		rootElem, htmlParseErr := parsers.ParseHTMLDoc(resp.Body)
-		if htmlParseErr != nil {
-			log.Fatal("Request Failure: ", err)
-			continue
-		}
-		hrefhandler.Handle(rootElem, pURL, rawURL)
-	}
-	log.Println(model.GetURLReg())
+	rawURLs := []string{rawURL}
+	crawler.MultiCrawler(rawURLs)
 }
