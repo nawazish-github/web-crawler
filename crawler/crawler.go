109b6712ab2159987d854633e5e04c1fb490e26e
mnawaz <mnawaz@thoughtworks.com>
2018-08-05 23:50:24 +0530

    Use generation type and provide MultiCrawler functionality

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/nawazish-github/web-crawler/httphelper"
 	"github.com/nawazish-github/web-crawler/model"
 
 	"github.com/nawazish-github/web-crawler/parsers"
@@ -17,31 +18,31 @@ import (
 //HrefHandler handles all the href tags
 //embedded in the given document and
 //updates the same to the urlRegistry
-var hrefHandler func(*html.Node, *url.URL, string)
+var hrefHandler func(*html.Node, *url.URL, string, *model.Generation)
 
 //Crawl iterates the HTML document beginning
 //at its root and updates the URL registry with
 //each encountered link.
-func Crawl(rootElem *html.Node, pURL *url.URL, rawURL string) {
-	hrefHandler = func(n *html.Node, pURL *url.URL, rawURL string) {
+func Crawl(rootElem *html.Node, pURL *url.URL, rawURL string, gen *model.Generation) {
+	hrefHandler = func(n *html.Node, pURL *url.URL, rawURL string, gen *model.Generation) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, atr := range n.Attr {
 				if atr.Key == "href" {
 					link := atr.Val
 					if isRelativeURL(link) {
 						link = constructAbsoluteURL(pURL, link)
-						updateURLRegistryWithLatestLink(rawURL, link)
+						updateURLRegistryWithLatestLink(rawURL, link, gen)
 						continue
 					}
-					verifyAndUpdateURLRegistryWithLatestLink(link, rawURL, pURL.Host)
+					verifyAndUpdateURLRegistryWithLatestLink(link, rawURL, pURL.Host, gen)
 				}
 			}
 		}
 		for elem := n.FirstChild; elem != nil; elem = elem.NextSibling {
-			hrefHandler(elem, pURL, rawURL)
+			hrefHandler(elem, pURL, rawURL, gen)
 		}
 	}
-	hrefHandler(rootElem, pURL, rawURL)
+	hrefHandler(rootElem, pURL, rawURL, gen)
 }
 func isRelativeURL(link string) bool {
 	if strings.HasPrefix(link, "/") {
@@ -54,10 +55,14 @@ func constructAbsoluteURL(pURL *url.URL, link string) string {
 	return pURL.Scheme + "://" + pURL.Host + link
 }
 
-func updateURLRegistryWithLatestLink(rawURL, link string) {
-	list := model.GetURLReg()[rawURL]
+func updateURLRegistryWithLatestLink(rawURL, link string, gen *model.Generation) {
+	list := gen.GenMap[rawURL]
 	list = append(list, link)
-	model.GetURLReg()[rawURL] = list
+	gen.GenMap[rawURL] = list
+	// list := model.GetURLReg()[rawURL]
+	// list = append(list, link)
+	// model.GetURLReg()[rawURL] = list
+
 	// list := urlReg[rawURL]
 	// list = append(list, link)
 	// urlReg[rawURL] = list
@@ -66,7 +71,7 @@ func updateURLRegistryWithLatestLink(rawURL, link string) {
 //verifyAndUpdateURLRegistryWithLatestLink checks if the link under
 //consideration is from the same base domain; it discards any
 //external links.
-func verifyAndUpdateURLRegistryWithLatestLink(link, rawURL, host string) {
+func verifyAndUpdateURLRegistryWithLatestLink(link, rawURL, host string, gen *model.Generation) {
 	pURL, err := parsers.ParseURL(link)
 	if err != nil {
 		log.Fatal("Could not parse the link: ", err)
@@ -75,10 +80,47 @@ func verifyAndUpdateURLRegistryWithLatestLink(link, rawURL, host string) {
 	if !strings.Contains(pURL.Host, host) {
 		return
 	}
-	updateURLRegistryWithLatestLink(rawURL, link)
+	updateURLRegistryWithLatestLink(rawURL, link, gen)
 }
 
-//GetURLReg returns the urlReg
-// func GetURLReg() map[string][]string {
-// 	return urlReg
-// }
+var siteMap []model.Generation
+var depth = 1
+var counter = 0
+
+//MultiCrawler crawls a list of URLs and returns a list of
+//generations.
+func MultiCrawler(rawURLs []string) []model.Generation {
+	counter++
+	if counter > depth {
+		return nil
+	}
+	gen := model.NewGeneration()
+	for _, rawURL := range rawURLs {
+		pURL, urlParseErr := parsers.ParseURL(rawURL)
+		if urlParseErr != nil {
+			log.Fatal("URL Parse Error: ", urlParseErr)
+			continue
+		}
+		//gen := model.NewGeneration()
+		gen.GenMap[rawURL] = []string{}
+		resp, err := httphelper.RequestTheURL(rawURL)
+		if err != nil {
+			log.Fatal("Request Failure: ", err)
+			continue
+		}
+		defer resp.Body.Close()
+		rootElem, htmlParseErr := parsers.ParseHTMLDoc(resp.Body)
+		if htmlParseErr != nil {
+			log.Fatal("Request Failure: ", err)
+			continue
+		}
+		Crawl(rootElem, pURL, rawURL, gen)
+		siteMap = append(siteMap, *gen)
+	}
+	for _, rawURLs := range gen.GenMap {
+		if len(rawURLs) > 0 {
+			MultiCrawler(rawURLs)
+		}
+	}
+	return siteMap
+}
