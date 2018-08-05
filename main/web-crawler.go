43b4d39bf683fd068d8148af0afdcac22b826482
mnawaz <mnawaz@thoughtworks.com>
2018-08-05 23:52:36 +0530

    Remove unnecessary vars from the main function

--- main/web-crawler.go
+++ main/web-crawler.go
@@ -6,11 +6,6 @@ import (
 	"github.com/nawazish-github/web-crawler/crawler"
 )
 
-var (
-	isFirstItr = true
-	urlReg     = make(map[string][]string)
-)
-
 func main() {
 	rawURL := os.Args[1]
 	rawURLs := []string{rawURL}
